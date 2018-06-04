17f34263e0664c93a89594fb19bd33f172c000b7
Michelle Olivieri <michelleo72@gmail.com>
2018-06-04 13:32:35 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validate :must_be_clickbaity
+
+  def must_be_clickbaity
+    if title && !title.include?("Won't Believe" || "Secret" || "Top" || "Guess")
+      errors.add(:title, "Must be clickbait-y")
+    end
+  end
+
 end
